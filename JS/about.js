735e324914951105ff771d4604a31c9a6002b36d
Vincent Costa <costa.vincent132@gmail.com>
2019-06-24 10:51:26 -0700

    Added a person factory, content is now generated from data instead of html

--- JS/about.js
+++ JS/about.js
@@ -1,1 +1,58 @@
-console.log("here")
+class PersonCard {
+    constructor(element, data) {
+        this.element = element;
+
+        this.data = data;
+
+        this.element.addEventListener("mouseover", _ => {
+
+        })
+    }
+
+
+}
+
+class PersonFactory {
+    constructor(data) {
+        this.people = [];
+
+        this.parent = document.querySelector(".people-container");
+
+        this.people = data.map(elemData => {
+            let element = this.constructHTML(elemData);
+
+            return new PersonCard(element, elemData);
+        })
+    }
+
+    constructHTML(data) {
+        let container = this.constructElement(this.parent, "div", "", [ "person" ]);
+        
+        this.constructElement(container, "img", "", ["person-img"]).src = data.img;
+
+        let contentDiv = this.constructElement(container, "div", "", ["person-content"]);
+
+        this.constructElement(contentDiv, "h3", data.name, []);
+        this.constructElement(contentDiv, "p", data.about, []);
+
+        return container;
+    }
+
+    constructElement(parent, type, data, classes) {
+        let element = document.createElement(type);
+        parent.appendChild(element);
+
+        if(data && data !== "") element.textContent = data;
+
+        classes.forEach(cssClass => element.classList.add(cssClass));
+
+        return element;
+    }
+}
+
+const data = [
+    { id: "1", name: "Vincent", img: "", about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi exercitationem repudiandae aliquid debitis obcaecati voluptas quos animi provident ipsam? Voluptates natus quasi mollitia illo ipsum, quis deserunt dolor aliquam quod." },
+    { id: "2", name: "Other", img: "", about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi exercitationem repudiandae aliquid debitis obcaecati voluptas quos animi provident ipsam? Voluptates natus quasi mollitia illo ipsum, quis deserunt dolor aliquam quod." }
+]
+
+const factory = new PersonFactory(data);
